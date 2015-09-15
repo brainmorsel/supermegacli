75325d7485afca5c5f1000d22d478b6d05c91911
The Invoice <zy@hoax.ws>
2015-09-15 11:57:22 +0800

    new commands

--- README.md
+++ README.md
@@ -3,20 +3,27 @@ SuperMegaCli
 
 Small sanity saving wrapper around LSI MegaCli.
 
-
     Usage:
         ./supermegacli.sh [options] {command}
 
     Options:
         -e=|--megacli-exe=PATH  Path to MegaCli executable.
         -a=|--adapter=N         Adapter number or "all" (default 0).
+        -h|--hide-headers       Don't show table headers.
 
     Commands:
-        pd show {list | DISK_ID...}  Physical drive info.
-        pd set hotspare DISK_ID...
-        pd set hotspare dedicated ARRAY DISK_ID...
+        help
+        pd list
+        pd count
+        pd info DISK_ID...           Physical drive info.
+        pd missing show
+        pd missing mark DISK_ID...
+        pd missing replace ARRAY ROW DISK_ID...
+        pd hotspare set DISK_ID...
+        pd hotspare dedicated ARRAY DISK_ID...
+        pd hotspare remove DISK_ID...
         vd show {list | DISK_ID...}  Virtual drive info.
-        adp show                     Show adapter info.
+        adp info                     Show adapter info.
         adp count                    Show adapters count.
         adp log                      Show adapter internal log.
 
--- supermegacli.sh
+++ supermegacli.sh
@@ -9,6 +9,7 @@ MEGA_EXE_VARIANTS=(
 )
 OWN_EXE="$0"
 
+
 help_usage()
 {
   cat >&2 <<- EOF
@@ -18,23 +19,33 @@ help_usage()
 	Options:
 	    -e=|--megacli-exe=PATH  Path to MegaCli executable.
 	    -a=|--adapter=N         Adapter number or "all" (default 0).
+	    -h|--hide-headers       Don't show table headers.
 
 	Commands:
-	    pd show {list | DISK_ID...}  Physical drive info.
-	    pd set hotspare DISK_ID...
-	    pd set hotspare dedicated ARRAY DISK_ID...
+	    help
+	    pd list
+	    pd count
+	    pd info DISK_ID...           Physical drive info.
+	    pd missing show
+	    pd missing mark DISK_ID...
+	    pd missing replace ARRAY ROW DISK_ID...
+	    pd hotspare set DISK_ID...
+	    pd hotspare dedicated ARRAY DISK_ID...
+	    pd hotspare remove DISK_ID...
 	    vd show {list | DISK_ID...}  Virtual drive info.
-	    adp show                     Show adapter info.
+	    adp info                     Show adapter info.
 	    adp count                    Show adapters count.
 	    adp log                      Show adapter internal log.
 	EOF
 }
+
 
 help_error()
 {
   echo -e "$1\n" >&2
   help_usage
 }
+
 
 main()
 {
@@ -51,6 +62,15 @@ main()
         ;;
       -a=*|--adapter=*)
         MEGA_CTL_ID="${opt#*=}"
+        shift
+        ;;
+      -h|--hide-headers)
+        OPT_HIDE_HEADERS=1
+        shift
+        ;;
+      -d|--dry-run)
+        OPT_DRY_RUN=1
+        OPT_HIDE_HEADERS=1
         shift
         ;;
       -*)
@@ -91,21 +111,23 @@ main()
 mega_cmd_pd()
 {
   case "$1" in
-    show)
-      if [[ -z "$2" || "$2" == "list" ]] ;then
-        mega_cmd_pd_show_list
-      else
-        shift
-        mega_cmd_pd_show_info "${@}"
-      fi
+    list)
+      mega_cmd_pd_list
       ;;
-    set)
-      shift
-      mega_cmd_pd_set "${@}"
+    count)
+      mega_cmd_pd_count
       ;;
-    rem|remove)
+    info)
       shift
-      mega_cmd_pd_remove "${@}"
+      mega_cmd_pd_info "${@}"
+      ;;
+    missing)
+      shift
+      mega_cmd_pd_missing "${@}"
+      ;;
+    hotspare)
+      shift
+      mega_cmd_pd_hotspare "${@}"
       ;;
     *)
       help_usage
@@ -114,31 +136,46 @@ mega_cmd_pd()
 }
 
 
-mega_cmd_pd_set()
+mega_cmd_pd_missing()
 {
   case "$1" in
-    hotspare)
+    show)
+      mega_cmd_pd_missing_show
+      ;;
+    mark)
       shift
-      mega_cmd_pd_set_hotspare "${@}"
+      mega_cmd_pd_missing_mark "${@}"
+      ;;
+    replace)
+      shift
+      mega_cmd_pd_missing_replace "${@}"
       ;;
     *)
       help_usage
       exit 1
-   esac
+  esac
 }
 
 
-mega_cmd_pd_set()
+mega_cmd_pd_hotspare()
 {
   case "$1" in
-    hotspare)
+    set)
       shift
-      mega_cmd_pd_set_hotspare "${@}"
+      mega_cmd_pd_hotspare_set "${@}"
+      ;;
+    dedicated)
+      shift
+      mega_cmd_pd_hotspare_dedicated "${@}"
+      ;;
+    remove)
+      shift
+      mega_cmd_pd_hotspare_remove "${@}"
       ;;
     *)
       help_usage
       exit 1
-   esac
+  esac
 }
 
 
@@ -162,7 +199,7 @@ mega_cmd_vd()
 mega_cmd_adp()
 {
   case "$1" in
-    show)
+    info)
       mega_cmd_adp_show_info
       ;;
     count)
@@ -202,23 +239,39 @@ mega_detect()
     return 1
 }
 
+mega_run()
+{
+  if [[ "$OPT_DRY_RUN" -eq 1 ]] ;then
+    echo ${MEGA_EXE} "$@" ${MEGA_FLAGS} 1>&2
+  else
+    ${MEGA_EXE} "$@"
+  fi
+}
+
 
 #------------------------------------------------------------------------------
 # Command handle functions.
 #------------------------------------------------------------------------------
-mega_cmd_pd_show_list()
+mega_cmd_pd_list()
 {
-  ${MEGA_EXE} -PDList -a${MEGA_CTL_ID} ${MEGA_FLAGS} | awk -F: '
+  mega_run -PDList -a${MEGA_CTL_ID} | awk -v hide_headers=$OPT_HIDE_HEADERS -F: '
   function ltrim(s) { sub(/^[ \t\r\n]+/, "", s); return s }
   function rtrim(s) { sub(/[ \t\r\n]+$/, "", s); return s }
   function trim(s)  { return rtrim(ltrim(s)); }
   function fmt_size(s) { split(s, a, " "); return a[1] a[2] }
   function fmt_temp(s) { split(s, a, " "); return a[1] }
+  function fmt_state(s) {
+    sub(", ", ":", s)
+    sub(" ", "", s)
+    return tolower(s)
+  }
   BEGIN {
     media_types["Solid State Device"] = "SSD"
     media_types["Hard Disk Device"] = "HDD"
 
-    print("ID\tType\tSize\t\tFW Ver\tMedia\tTemp\tErr\tPFC\tState")
+    if (!hide_headers) {
+      print("ID\tType\tSize\t\tFW Ver\tMedia\tTemp\tErr\tPFC\tState")
+    }
   }
   {
     key = trim($1)
@@ -233,7 +286,7 @@ mega_cmd_pd_show_list()
       temp = fmt_temp(d["Drive Temperature"])
       err = d["Media Error Count"] "/" d["Other Error Count"]
       pfc = d["Predictive Failure Count"]
-      state = d["Firmware state"]
+      state = fmt_state(d["Firmware state"])
 
       print( dev_id "\t" pd_type "\t" pd_size "\t" fw_ver "\t" media "\t" temp "\t" err "\t" pfc "\t" state)
       delete d
@@ -247,42 +300,82 @@ mega_cmd_pd_show_list()
 }
 
 
-mega_cmd_pd_show_info()
+mega_cmd_pd_info()
 {
   local pd_ids="$@"
-  ${MEGA_EXE} -PDInfo -PhysDrv "[${pd_ids// /,}]" -a${MEGA_CTL_ID} ${MEGA_FLAGS}
+  mega_run -PDInfo -PhysDrv "[${pd_ids// /,}]" -a${MEGA_CTL_ID}
 }
 
 
-mega_cmd_pd_set_hotspare()
+mega_cmd_pd_count()
 {
-  if [[ "$1" == "dedicated" ]] ;then
+  mega_run -PDGetNum -a${MEGA_CTL_ID} | awk -F': ' '
+  /Number of Physical Drives/ { print $2 }
+  '
+}
+
+
+mega_cmd_pd_missing_show()
+{
+  mega_run -PdGetMissing -a${MEGA_CTL_ID}
+}
+
+
+mega_cmd_pd_missing_mark()
+{
+  local pd_ids="$@"
+  mega_run -PdMarkMissing -PhysDrv "[${pd_ids// /,}]" -a${MEGA_CTL_ID}
+}
+
+
+mega_cmd_pd_missing_replace()
+{
+  local array="$1"
+  local row="$2"
+  shift 2
+  local pd_ids="$@"
+  mega_run -PdReplaceMissing -PhysDrv "[${pd_ids// /,}]" -Array $array -Row $row -a${MEGA_CTL_ID}
+}
+
+
+mega_cmd_pd_hotspare_set()
+{
+  local pd_ids="$@"
+  mega_run -PDHSP -Set -PhysDrv "[${pd_ids// /,}]" -a${MEGA_CTL_ID}
+}
+
+
+mega_cmd_pd_hotspare_dedicated()
+{
     local array_id="$2"
-    shift 2
+    shift
     local pd_ids="$@"
-    ${MEGA_EXE} -PDHSP -Set -Dedicated -Array${array_id} -PhysDrv "[${pd_ids// /,}]" -a${MEGA_CTL_ID} ${MEGA_FLAGS}
-  else
-    local pd_ids="$@"
-    ${MEGA_EXE} -PDHSP -Set -PhysDrv "[${pd_ids// /,}]" -a${MEGA_CTL_ID} ${MEGA_FLAGS}
-  fi
+    mega_run -PDHSP -Set -Dedicated -Array${array_id} -PhysDrv "[${pd_ids// /,}]" -a${MEGA_CTL_ID}
+}
+
+
+mega_cmd_pd_hotspare_remove()
+{
+  local pd_ids="$@"
+  mega_run -PDHSP -Rmv -PhysDrv "[${pd_ids// /,}]" -a${MEGA_CTL_ID}
 }
 
 
 mega_cmd_vd_show_info()
 {
-  ${MEGA_EXE} -LDInfo -L"$1" -a${MEGA_CTL_ID} ${MEGA_FLAGS}
+  mega_run -LDInfo -L"$1" -a${MEGA_CTL_ID}
 }
 
 
 mega_cmd_adp_show_info()
 {
-  ${MEGA_EXE} -AdpAllInfo -a${MEGA_CTL_ID} ${MEGA_FLAGS}
+  mega_run -AdpAllInfo -a${MEGA_CTL_ID}
 }
 
 
 mega_cmd_adp_show_count()
 {
-  ${MEGA_EXE} -adpCount ${MEGA_FLAGS} | awk '
+  mega_run -adpCount | awk '
   /Controller Count/ { sub(/[^0-9]+/, "", $3); print $3 }
   '
 }
@@ -290,7 +383,7 @@ mega_cmd_adp_show_count()
 
 mega_cmd_adp_show_log()
 {
-  ${MEGA_EXE} -AdpAlILog -a${MEGA_CTL_ID} ${MEGA_FLAGS}
+  mega_run -AdpAlILog -a${MEGA_CTL_ID}
 }
 
 
